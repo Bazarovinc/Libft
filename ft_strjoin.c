fc4eca2e5dda93244b745c67685eee21e0cd5463
Crius Telma <ctelma@su-h1.21-school.ru>
2019-09-13 15:23:49 +0300

    makefile, c files with functions, author

---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctelma <ctelma@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/08 16:07:52 by ctelma            #+#    #+#             */
+/*   Updated: 2019/09/12 14:35:52 by ctelma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	ft_strcat(s, s2);
+	return (s);
+}
